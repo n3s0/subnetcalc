6836a1ac5a1d808f411e69647a53f5dedce2a680
Timothy Loftus <tloftus@protonmail.com>
2024-10-06 20:50:00 -0500

    trying to fix commit

--- snc/subnetCmd.go
+++ snc/subnetCmd.go
@@ -4,21 +4,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
-<<<<<<< HEAD
-var subnetCmd = &cobra.Command {
-  Use: "subnet [subnet/CIDR]",
-  Short: "NOT COMPLETE: Calculate and display subnet and subnet ranges",
-  Long: "Calculate and display subnet and subnet ranges.",
-  Run: func(cmd *cobra.Command, args []string) {
-  },
-=======
 var subnetCmd = &cobra.Command{
 	Use:   "subnet [subnet/CIDR]",
-	Short: "Calculate and display subnet and subnet ranges",
+	Short: "NOT COMPLETE: Calculate and display subnet and subnet ranges",
 	Long:  "Calculate and display subnet and subnet ranges.",
 	Run: func(cmd *cobra.Command, args []string) {
 	},
->>>>>>> 8a102b8 (Updating subnet calculator feature)
 }
 
 func init() {
